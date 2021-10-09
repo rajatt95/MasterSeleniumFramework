7b7ed39067feef36331c42aef20bed044d22dad0
Rajat Verma <rajatvermaa95@gmail.com>
2021-10-09 07:29:33 +0530

    Update TestConfig.java

--- src/main/java/org/selenium/java_Mail_API/TestConfig.java
+++ src/main/java/org/selenium/java_Mail_API/TestConfig.java
@@ -10,16 +10,12 @@ public class TestConfig {
 	private static String server = "smtp.gmail.com";
 	private static String port = "587";
 
-	private static String from = "testtmail95@gmail.com";
-	private static String password = "HelloSher";
+	private static String from = "************";
+	private static String password = "**************";
 
-	private static String[] to = { /* "rajatvermaa95@gmail.com", */ "testtmail95@gmail.com" };
+	private static String[] to = { /* "**************@gmail.com", */ "***********@gmail.com" };
 
-	/*
-	 * public static String[] to = { "rajatvermaa95@gmail.com",
-	 * "testtmail95@gmail.com", "vermanishant93@gmail.com", "GAUTYVERMA@gmail.com",
-	 * "skv563.sv@gmail.com" };
-	 */
+	
 
 	private static String subject = FrameworkConstants.getProjectName() + " - Master Selenium Framework";
 
