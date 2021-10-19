338d1890c3c553940e755c289df183bdb6f108c4
Rajat Verma <rajatvermaa95@gmail.com>
2021-10-19 17:25:49 +0530

    Update EmailConfig.java

--- src/main/java/org/selenium/java_Mail_API/EmailConfig.java
+++ src/main/java/org/selenium/java_Mail_API/EmailConfig.java
@@ -11,7 +11,7 @@ public class EmailConfig {
 	public static final String PORT = "587";
 
 	public static final String FROM = "testtmail95@gmail.com";
-	public static final String PASSWORD = "HelloSher";
+	public static final String PASSWORD = "**********";
 
 	/* "**********@gmail.com", */
 	public static final String[] TO = {"testtmail95@gmail.com"};
